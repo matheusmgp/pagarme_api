a3c6f965964277d7b86f14f3dde912fc23115428
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-16 13:27:35 -0300

    FEAT: Repositories

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { routes } = require('./routes');
 
 app.use(cors());
 app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 module.exports = app;
---
+++ src/repositories/payable.repository.js
@@ -0,0 +1,19 @@
+const PrismaService = require('../services/prisma/prisma.service');
+
+const _prismaService = PrismaService;
+class PayableRepository {
+  async create(payload) {
+    return _prismaService.payable.create({
+      data: payload,
+    });
+  }
+  async getAll() {
+    return await _prismaService.payable.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+    });
+  }
+}
+
+module.exports = PayableRepository;
