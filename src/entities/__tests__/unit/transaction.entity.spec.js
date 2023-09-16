85746df5ac060cf83dac5a2cac455aed4821d6cb
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-16 15:44:28 -0300

    FEAT: transactionEntityFactory UNIT test

--- src/entities/__tests__/unit/transaction.entity.spec.js
+++ src/entities/__tests__/unit/transaction.entity.spec.js
@@ -1,7 +1,8 @@
-const TransactionEntity = require('../../transaction.entity');
+const { TransactionEntity, transactionEntityFactory } = require('../../transaction.entity');
 
 describe('TransactionEntity unit tests', () => {
   let sut;
+
   let expires_date = new Date();
   beforeEach(() => {
     sut = new TransactionEntity(
@@ -28,5 +29,24 @@ describe('TransactionEntity unit tests', () => {
   });
   it('maskCardNumber  method', () => {
     expect(sut.maskCardNumber()).toStrictEqual('*******8910');
+  });
+  it('should return a instance of TransactionEntity', () => {
+    const result = transactionEntityFactory({
+      price: 100,
+      description: 'description',
+      payment_method: 'debit_card',
+      card_number: '132231321',
+      owner_name: 'matheus',
+      card_expires_date: new Date(),
+      cvv: 123,
+    });
+    expect(result).toBeInstanceOf(TransactionEntity);
+    expect(result.price).toEqual(100);
+    expect(result.description).toEqual('description');
+    expect(result.payment_method).toEqual('debit_card');
+    expect(result.card_number).toEqual('132231321');
+    expect(result.owner_name).toEqual('matheus');
+    expect(result.card_expires_date).toBeInstanceOf(Date);
+    expect(result.cvv).toEqual(123);
   });
 });
