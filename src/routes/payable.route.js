79bf0ec9505c8fbdf1ec1c22a74e3ae16cdf4a69
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-18 20:41:56 -0300

    REFACTOR:Refactoring route and controller

--- src/routes/payable.route.js
+++ src/routes/payable.route.js
@@ -1,32 +1,10 @@
 const { Router } = require('express');
-const BaseError = require('../errors/base-error');
-const httpStatusCodes = require('../errors/status-code/http-status-code');
 const PayableController = require('../controllers/payable/payable.controller');
-const { responseHttpSuccess, responseHttpException } = require('../presenters/httpResponse');
 
 const routePayable = Router();
 const _controller = PayableController;
 
-routePayable.get('/api/v1/dev/payable', async (req, res) => {
-  try {
-    responseHttpSuccess(await _controller.getAll(), res, req);
-  } catch (err) {
-    if (err instanceof BaseError) {
-      responseHttpException(err.message, req.method, res, err.statusCode);
-    } else {
-      responseHttpException(err.message, req.method, res, httpStatusCodes.INTERNAL_SERVER);
-    }
-  }
-});
-routePayable.get('/api/v1/dev/payable/info', async (req, res) => {
-  try {
-    responseHttpSuccess(await _controller.getAllInfo(), res, req);
-  } catch (err) {
-    if (err instanceof BaseError) {
-      responseHttpException(err.message, req.method, res, err.statusCode);
-    } else {
-      responseHttpException(err.message, req.method, res, httpStatusCodes.INTERNAL_SERVER);
-    }
-  }
-});
+routePayable.get('/api/v1/dev/payable', _controller.getAll);
+routePayable.get('/api/v1/dev/payable/info', _controller.getAllInfo);
+
 module.exports = routePayable;
