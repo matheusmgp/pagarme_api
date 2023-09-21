4fe1287da659dd9dbad9f47bf8d2c6b6e7888057
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-21 13:28:54 -0300

    FIX: Tests

--- src/services/__tests__/e2e/transaction.service.e2e-spec.js
+++ src/services/__tests__/e2e/transaction.service.e2e-spec.js
@@ -22,7 +22,7 @@ describe('Transaction e2e tests', () => {
     await PrismaService.payable.deleteMany();
     await PrismaService.transaction.deleteMany();
   });
-  it('should create a Transaction and Payable ', async () => {
+  it('should create a Transaction and Payable', async () => {
     const response = await request(app)
       .post('/api/v1/dev/transaction')
       .send(entity)
@@ -64,6 +64,30 @@ describe('Transaction e2e tests', () => {
     expect(response.body).toStrictEqual({
       statusCode: 200,
       data: { available: 97, waiting_funds: 0 },
+      timestamp: expect.any(String),
+      method: 'GET',
+    });
+  });
+  it('should return the transactions', async () => {
+    await PrismaService.transaction.create({
+      data: { ...entity, cvv: parseInt(entity.cvv.toString()) },
+    });
+    const response = await request(app).get('/api/v1/dev/transaction');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toStrictEqual({
+      statusCode: 200,
+      data: [
+        {
+          id: expect.any(Number),
+          price: 100,
+          description: 'Smartband XYZ 3.0',
+          payment_method: 'debit_card',
+          card_number: '*******8910',
+          owner_name: 'matheus',
+          card_expires_date: expect.any(String),
+          cvv: 855,
+        },
+      ],
       timestamp: expect.any(String),
       method: 'GET',
     });
